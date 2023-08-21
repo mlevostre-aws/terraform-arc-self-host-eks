beae465f1294bc1191271ea97c47a52053873c31
mlevos <mathias.levostre@gmail.com>
2023-08-21 21:37:55 +0200

    feat(): init project

---
+++ versions.tf
@@ -0,0 +1,16 @@
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.eks_cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.eks_cluster.token
+  experiments {
+    manifest_resource = true
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.eks_cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.eks_cluster.token
+  }
+}
