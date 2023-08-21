00bc7e52a68ec58603f180db66a148c6cdf6a857
Mathias LEVOSTRE <mathias.levostre@gmail.com>
2023-08-21 23:03:39 +0200

    chore(): test (#5)

--- kubernetes.tf
+++ kubernetes.tf
@@ -5,14 +5,14 @@ resource "kubernetes_manifest" "self_host_github_runner" {
     metadata = {
       name      = "aws-self-host"
       namespace = var.github_namespace_name
-      
+
     }
     spec = {
       replicas = 1
       template = {
         spec = {
           organization = "mlevos-demo"
-          labels       = ["aws"]
+          labels       = ["aws", "test"]
         }
       }
     }
